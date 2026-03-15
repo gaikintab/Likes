f9536cc9e155d77b34e86fa73b37670e80e512df
Gaikin <gaikintab@gmail.com>
2026-03-15 13:13:22 +0300

    Add Program code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
 package ru.netology
 
 fun main() {
-    
+    val likes = 21       // кол-во лайков
+
+    val endOfNumber = likes % 10
+    val people = if (endOfNumber == 1 && likes != 11) " человеку" else " людям"
+
+    println("Понравилось $likes$people")
 }
